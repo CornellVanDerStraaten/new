05ae5c79316910dbc0c3be64a9d6d4d81a4407fd
Cornell van der Straaten <cornellbvanders@gmail.com>
2024-02-19 21:46:24 +0000

    Remove all

--- resources/views/components/faq-item.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <dt class="text-base font-semibold leading-7 text-gray-900">{{ $question }}</dt>
-    <dd class="mt-2 text-base leading-7 text-gray-600">{{ $slot }}</dd>
-</div>
--- resources/views/livewire/goal-progress-report.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <h2 class="text-2xl">{{ $progressReport->name }}</h2>
-</div>
